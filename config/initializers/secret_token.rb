d7e79e06de6ec9290e9f65abbce1b397335353b7
Brian Watson <brian.a.watson@gmail.com>
2013-12-18 15:36:22 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Igd::Application.config.secret_key_base = '16898fc01e45df724495342049bcfeafe02e6a6b4732353dc138fe0b431ba1d7cfaf7b4565aa2519b48c05e3914b2b8c46c9797b0084c2d88b0c52aaf2a8ec56'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Igd::Application.config.session_store :cookie_store, key: '_igd_session'
